4827afcaacc03f96624a111f06743f62cff6aacd
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-05-24 17:18:39 +0100

    Fixed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [] ;
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -18,13 +20,15 @@ app.get("/", function (req, res) {
 
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day });  
+  res.render("list", { kindOfDay: day, newListItems: items});  
 
 });
 
 app.post("/", function(req, res){
-      var item = req.body.newItem
-      console.log(item);
+     var item = req.body.newItem
+     items.push(item);
+      
+      res.redirect("/");
 });
 
 app.listen(3000, function () {
